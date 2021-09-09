16af78d56b0172d81bdb3f265e1a0d3c3b198d9a
Walter Hinojosa <88406709+YahirH@users.noreply.github.com>
2021-09-09 11:48:26 -0500

    Add files via upload

---
+++ Practica2/Script.ps1
@@ -0,0 +1,15 @@
+﻿Function TestPoShArgs {
+"Me encanta PowerShell $Args"
+}
+TestPoShArgs un monton !!
+
+$servicios= Get-Service
+foreach($elemento in $servicios){
+Write-host $elemento.Name "-->" $elemento.status
+}
+
+
+if($servicios){
+ 
+   Write-Host "Se ha realizado con exito"
+}
